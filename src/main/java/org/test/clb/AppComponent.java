39cfc38fc908221403f135752b617d0521e2a3d3
Ratul Sikder <ratulsikder121@gmail.com>
2022-09-15 01:47:05 +0600

    Java socket programming for sending data (priliminary)

--- src/main/java/org/test/clb/AppComponent.java
+++ src/main/java/org/test/clb/AppComponent.java
@@ -19,136 +19,140 @@ import org.slf4j.LoggerFactory;
 @Component(immediate = true)
 public class AppComponent {
 
-	private final Logger log = LoggerFactory.getLogger(getClass());
-	@Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
-	protected DeviceService deviceService;
-	@Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
-	protected MastershipStore mastershipStore;
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected DeviceService deviceService;
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected MastershipStore mastershipStore;
 
-	// Timer to run Monitoring Module
-	Timer timer = new Timer();
+    // Timer to run Monitoring Module
+    Timer timer = new Timer();
 
-	@Activate
-	protected void activate() {
+    @Activate
+    protected void activate() {
 
-		// Controller node(IP) Declaration
-		NodeId node1 = new NodeId("172.17.0.5");
-		NodeId node2 = new NodeId("172.17.0.6");
-		NodeId node3 = new NodeId("172.17.0.7");
-
-		// ArrayList to store Controller object
-		ArrayList<Controller> controllers = new ArrayList<>();
-		
-		// Creating Controller instances and assigning to Array List
-		controllers.add(new Controller(node1));
-		controllers.add(new Controller(node2));
-		controllers.add(new Controller(node3));
-
-		// Getting all Switches(devices) of the Network
-		Iterable<Device> devices = deviceService.getDevices();
-
-		// *******Starting Monitoring Module (within TimerTask)********
-		TimerTask task = new TimerTask() {
-			@Override
-			public void run() {
-				log.info("*** Starts Reporting ***");
-
-				// Initially set Controllers load and its Switches list to zero and blank
-				for (Controller controller : controllers) {
-					controller.controllerLoad = 0;
-					controller.switches.clear();
-				}
-
-				// Traversing each Switch (Device) for gathering port delta statistics
-				/* Port delta statistics is the difference between previous and current 
-				 * traffic of a port of a switch within two reporting time to the controller.
-				 * The load of a controller is proportionate to the gross traffic of its
-				 * connected switches(devices)
-				*/
-				for (Device d : devices) {
-					List<Port> ports = deviceService.getPorts(d.id());
-					long bytes = 0;
-					for (Port port : ports) {
-						PortStatistics portstat = deviceService.getDeltaStatisticsForPort(d.id(), port.number());
-						if (portstat != null) {
-							bytes += portstat.bytesReceived();
-						}
-					}
-
-					/*
-					 * Aggregating traffic load of switches to controller and
-					 * adding switch to the corresponding controller object/
-					 */
-					for (Controller controller : controllers) {
-						// log.info(String.valueOf(mastershipStore.getMaster(d.id())));
-						if (controller.nodeId.equals(mastershipStore.getMaster(d.id()))) {
-							controller.controllerLoad += bytes;
-							controller.addSwitch(d.id(), bytes);
-						}
-					}
-				}
+        //Java socket for sending data
+        Client client = new Client("192.168.1.100", 5000);
 
 
-				//Calculating Average Controller Load
-				long averageControllerLoad;
-				long temp = 0;
-				for(Controller controller: controllers){
-					temp += controller.controllerLoad;
-				}
-				averageControllerLoad = (long)(temp/controllers.size());
+        // Controller node(IP) Declaration
+        NodeId node1 = new NodeId("172.17.0.5");
+        NodeId node2 = new NodeId("172.17.0.6");
+        NodeId node3 = new NodeId("172.17.0.7");
 
-				/*
-				 * Controller Overload Check
-				 * Detecting Overloaded Controller
-				 * Declaring Load Balancing(controller overload) Threshold
-				 * Future: Dynamic LB Threshold can be implemented
-				 */
-				final long loadBalancingThreshold = 15000;
-				Controller overloadedController = null;
-				//Sort Controller Arraylist wrt Controller Load(Reverse)
-				//ArrayList<Controller> sortedControllers = controllers;
-				Collections.sort(controllers, Comparator.comparing(Controller::getControllerLoad).reversed());
-				if(controllers.get(0).controllerLoad > loadBalancingThreshold){
-					overloadedController = controllers.get(0);
-				}
+        // ArrayList to store Controller object
+        ArrayList<Controller> controllers = new ArrayList<>();
+
+        // Creating Controller instances and assigning to Array List
+        controllers.add(new Controller(node1));
+        controllers.add(new Controller(node2));
+        controllers.add(new Controller(node3));
+
+        // Getting all Switches(devices) of the Network
+        Iterable<Device> devices = deviceService.getDevices();
+
+        // *******Starting Monitoring Module (within TimerTask)********
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                log.info("*** Starts Reporting ***");
+
+                // Initially set Controllers load and its Switches list to zero and blank
+                for (Controller controller : controllers) {
+                    controller.controllerLoad = 0;
+                    controller.switches.clear();
+                }
+
+                // Traversing each Switch (Device) for gathering port delta statistics
+                /* Port delta statistics is the difference between previous and current
+                 * traffic of a port of a switch within two reporting time to the controller.
+                 * The load of a controller is proportionate to the gross traffic of its
+                 * connected switches(devices)
+                 */
+                for (Device d : devices) {
+                    List<Port> ports = deviceService.getPorts(d.id());
+                    long bytes = 0;
+                    for (Port port : ports) {
+                        PortStatistics portstat = deviceService.getDeltaStatisticsForPort(d.id(), port.number());
+                        if (portstat != null) {
+                            bytes += portstat.bytesReceived();
+                        }
+                    }
+
+                    /*
+                     * Aggregating traffic load of switches to controller and
+                     * adding switch to the corresponding controller object/
+                     */
+                    for (Controller controller : controllers) {
+                        // log.info(String.valueOf(mastershipStore.getMaster(d.id())));
+                        if (controller.nodeId.equals(mastershipStore.getMaster(d.id()))) {
+                            controller.controllerLoad += bytes;
+                            controller.addSwitch(d.id(), bytes);
+                        }
+                    }
+                }
+
+
+                //Calculating Average Controller Load
+                long averageControllerLoad;
+                long temp = 0;
+                for (Controller controller : controllers) {
+                    temp += controller.controllerLoad;
+                }
+                averageControllerLoad = (long) (temp / controllers.size());
+
+                /*
+                 * Controller Overload Check
+                 * Detecting Overloaded Controller
+                 * Declaring Load Balancing(controller overload) Threshold
+                 * Future: Dynamic LB Threshold can be implemented
+                 */
+                final long loadBalancingThreshold = 15000;
+                Controller overloadedController = null;
+                //Sort Controller Arraylist wrt Controller Load(Reverse)
+                //ArrayList<Controller> sortedControllers = controllers;
+                Collections.sort(controllers, Comparator.comparing(Controller::getControllerLoad).reversed());
+                if (controllers.get(0).controllerLoad > loadBalancingThreshold) {
+                    overloadedController = controllers.get(0);
+                }
 
 				/*
 				Beginning Controller Selection Module
 				 */
-				Controller selectedController = null;
-				if(overloadedController!=null){
-					//Sort controller ascending order wrt load
-					Collections.sort(controllers, Comparator.comparing(Controller::getControllerLoad));
+                Controller selectedController = null;
+                if (overloadedController != null) {
+                    //Sort controller ascending order wrt load
+                    Collections.sort(controllers, Comparator.comparing(Controller::getControllerLoad));
 					/*
 					Getting the least loaded controller as selected controller for migration
 					Only load factor considered. Future add: Latency factor
 					 */
-					selectedController = controllers.get(0);
-					
-				}
+                    selectedController = controllers.get(0);
 
-				//Re-sort controller objects wrt ip order(node address)
-				Collections.sort(controllers,Comparator.comparing(Controller::getNodeId));
+                }
+
+                //Re-sort controller objects wrt ip order(node address)
+                Collections.sort(controllers, Comparator.comparing(Controller::getNodeId));
 
 				/*
 				Beginning Switch Selection Module
 				Select the most loaded switch from the overloaded controller
 				 */
-				Switch selectedSwitch = null;
-				if( (overloadedController!=null) && (selectedController!=null) ){
-					//Sort switch arraylist of overloaded controller object(descending)
-					Collections.sort(overloadedController.switches,Comparator.comparing(Switch::getSwitchLoad).reversed());
-					selectedSwitch = overloadedController.switches.get(0);
-				}
+                Switch selectedSwitch = null;
+                if ((overloadedController != null) && (selectedController != null)) {
+                    //Sort switch arraylist of overloaded controller object(descending)
+                    Collections.sort(overloadedController.switches, Comparator.comparing(Switch::getSwitchLoad).reversed());
+                    selectedSwitch = overloadedController.switches.get(0);
+                }
 
 
-				//Print the overloaded and selected controller and selected switch
-				try{
-					log.info("Overloaded Controller: "+overloadedController.nodeId.toString()+ " Selected Controller: "+selectedController.nodeId.toString()+
-							" Selected Switch: "+selectedSwitch.getDeviceId().toString());
-				}catch (NullPointerException exception){
-					log.info("No overloaded controllers.");
-				}
+                //Print the overloaded and selected controller and selected switch
+                try {
+                    log.info("Overloaded Controller: " + overloadedController.nodeId.toString() + " Selected Controller: " + selectedController.nodeId.toString() +
+                            " Selected Switch: " + selectedSwitch.getDeviceId().toString());
+                } catch (NullPointerException exception) {
+                    log.info("No overloaded controllers.");
+                }
 
 				/*
 				//Test print
@@ -158,60 +162,58 @@ public class AppComponent {
 
 				 */
 
-				// For testing...
-				// Retrieving info of each controller and display in ONOS log
-				for (Controller controller : controllers) {
-					ArrayList<Switch> switches = controller.getSwitches();
-					int numberOfSwitch = switches.size();
-					String switchId[] = new String[numberOfSwitch];
-					long switchLoad[] = new long[numberOfSwitch];
-					int i = 0;
-					for (Switch aSwitch : switches) {
-						switchId[i] = aSwitch.deviceId.toString();
-						switchLoad[i] = aSwitch.switchLoad;
-						i++;
-					}
-					log.info("* " + controller.nodeId + " Load: " + controller.controllerLoad + " Switches: "
-							+ switches.size()+" -> "+ Arrays.toString(switchId) + " Switch Load: " + Arrays.toString(switchLoad));
-				}
+                // For testing...
+                // Retrieving info of each controller and display in ONOS log
+                for (Controller controller : controllers) {
+                    ArrayList<Switch> switches = controller.getSwitches();
+                    int numberOfSwitch = switches.size();
+                    String switchId[] = new String[numberOfSwitch];
+                    long switchLoad[] = new long[numberOfSwitch];
+                    int i = 0;
+                    for (Switch aSwitch : switches) {
+                        switchId[i] = aSwitch.deviceId.toString();
+                        switchLoad[i] = aSwitch.switchLoad;
+                        i++;
+                    }
+                    log.info("* " + controller.nodeId + " Load: " + controller.controllerLoad + " Switches: "
+                            + switches.size() + " -> " + Arrays.toString(switchId) + " Switch Load: " + Arrays.toString(switchLoad));
+                }
 
 				/*
 				Starting Migration Module
 				Future: Migration failure will be tracked and avoided by controller selection and switch selection module
 				 */
-				if( (overloadedController!=null) && (selectedController!=null) && (selectedSwitch!=null) ){
-					//Reassigning switch
-					mastershipStore.setMaster(selectedController.nodeId, selectedSwitch.deviceId);
-					//Updating load info of controllers
-					overloadedController.controllerLoad -= selectedSwitch.switchLoad;
-					selectedController.controllerLoad += selectedSwitch.switchLoad;
-					//Removing the first element of the switch list. The first element is the switch selected and the list is sorted descending order.
-					overloadedController.switches.remove(0);
-					//Adding switch to the new controller(optional)
-					selectedController.switches.add(selectedSwitch);
-					log.info("Switch Reassigned: "+ selectedSwitch.deviceId.toString()+" -> "+selectedController.nodeId.toString());
-				}
+                if ((overloadedController != null) && (selectedController != null) && (selectedSwitch != null)) {
+                    //Reassigning switch
+                    mastershipStore.setMaster(selectedController.nodeId, selectedSwitch.deviceId);
+                    //Updating load info of controllers
+                    overloadedController.controllerLoad -= selectedSwitch.switchLoad;
+                    selectedController.controllerLoad += selectedSwitch.switchLoad;
+                    //Removing the first element of the switch list. The first element is the switch selected and the list is sorted descending order.
+                    overloadedController.switches.remove(0);
+                    //Adding switch to the new controller(optional)
+                    selectedController.switches.add(selectedSwitch);
+                    log.info("Switch Reassigned: " + selectedSwitch.deviceId.toString() + " -> " + selectedController.nodeId.toString());
+                }
 
 
-
-
-
-				// For testing...
-				// Retrieving info of each controller and display in ONOS log
-				for (Controller controller : controllers) {
-					ArrayList<Switch> switches = controller.getSwitches();
-					int numberOfSwitch = switches.size();
-					String switchId[] = new String[numberOfSwitch];
-					long switchLoad[] = new long[numberOfSwitch];
-					int i = 0;
-					for (Switch aSwitch : switches) {
-						switchId[i] = aSwitch.deviceId.toString();
-						switchLoad[i] = aSwitch.switchLoad;
-						i++;
-					}
-					log.info("# " + controller.nodeId + " Load: " + controller.controllerLoad + " Switches: "
-							+ switches.size()+" -> "+ Arrays.toString(switchId) + " Switch Load: " + Arrays.toString(switchLoad));
-				}
+                // For testing...
+                // Retrieving info of each controller and display in ONOS log
+                for (Controller controller : controllers) {
+                    ArrayList<Switch> switches = controller.getSwitches();
+                    int numberOfSwitch = switches.size();
+                    String switchId[] = new String[numberOfSwitch];
+                    long switchLoad[] = new long[numberOfSwitch];
+                    int i = 0;
+                    for (Switch aSwitch : switches) {
+                        switchId[i] = aSwitch.deviceId.toString();
+                        switchLoad[i] = aSwitch.switchLoad;
+                        i++;
+                    }
+                    log.info("# " + controller.nodeId + " Load: " + controller.controllerLoad + " Switches: "
+                            + switches.size() + " -> " + Arrays.toString(switchId) + " Switch Load: " + Arrays.toString(switchLoad));
+                    client.sendData("timer task data sent");
+                }
 
 
 				/*
@@ -222,11 +224,10 @@ public class AppComponent {
 				 */
 
 
-				//Checking Controller Overload
+                //Checking Controller Overload
 
 
-
-				// Testing switch reassignment...
+                // Testing switch reassignment...
 				/*
 				boolean test = true;
 				for (Controller controller : controllers) {
@@ -241,17 +242,17 @@ public class AppComponent {
 
 				 */
 
-			}
-		};
-		// Timer delay 10 seconds; recommended 3 seconds(from ONOS documentation)
-		timer.scheduleAtFixedRate(task, 0, 10000);
+            }
+        };
+        // Timer delay 10 seconds; recommended 3 seconds(from ONOS documentation)
+        timer.scheduleAtFixedRate(task, 0, 10000);
 
-	}
+    }
 
-	@Deactivate
-	protected void deactivate() {
-		timer.cancel();
-		log.info("Stopped");
-	}
+    @Deactivate
+    protected void deactivate() {
+        timer.cancel();
+        log.info("Stopped");
+    }
 
 }
---
+++ src/main/java/org/test/clb/Client.java
@@ -0,0 +1,63 @@
+package org.test.clb;// A Java program for a Client
+import java.net.*;
+import java.io.*;
+
+public class Client
+{
+    // initialize socket and input output streams
+    private Socket socket		 = null;
+    private DataInputStream input = null;
+    private DataOutputStream out	 = null;
+
+    // constructor to put ip address and port
+    public Client(String address, int port)
+    {
+        // establish a connection
+        try
+        {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            // takes input from terminal
+            input = new DataInputStream(System.in);
+
+            // sends output to the socket
+            out = new DataOutputStream(socket.getOutputStream());
+        }
+        catch(UnknownHostException u)
+        {
+            System.out.println(u);
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+    }
+
+    public void sendData(String data){
+        try
+        {
+            out.writeUTF(data);
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+
+    }
+
+    public void connectionClose(){
+        // close the connection
+        try
+        {
+            input.close();
+            out.close();
+            socket.close();
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+    }
+
+}
